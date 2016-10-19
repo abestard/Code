6f52e1bf2cf3fc024a15c3d4fdcb86d4f8154238
abestard <bestard.rivera@gmail.com>
2016-10-19 13:09:23 -0400

    Add files via upload

---
+++ Java Template.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner cin = new Scanner(System.in);
+		int a, b;
+		b = cin.nextInt(); a = cin.nextInt();
+        System.out.printf("%d", a+b);
+		cin.close();
+	}
+}
